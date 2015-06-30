5d642de31a7c0b2fe86e2e3c2b65b6a37200b248
frodo <frodo@z-inc.net>
2015-06-30 15:09:41 +0500

    added Item model

---
+++ app/src/main/java/ru/unatco/rss/model/Item.java
@@ -0,0 +1,22 @@
+package ru.unatco.rss.model;
+
+public class Item {
+    private String mTitle;
+    private String mDescription;
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+}
