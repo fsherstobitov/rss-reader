22b1a9c6d21f4c587e5a1f9cd28cc0ba0d35eb28
frodo <frodo@z-inc.net>
2015-06-30 15:51:37 +0500

    implemented Parcelable in Subscription

--- app/src/main/java/ru/unatco/rss/model/Subscription.java
+++ app/src/main/java/ru/unatco/rss/model/Subscription.java
@@ -1,8 +1,31 @@
 package ru.unatco.rss.model;
 
-public class Subscription {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Subscription implements Parcelable {
+
+    public static final Creator<Subscription> CREATOR = new Creator<Subscription>() {
+        @Override
+        public Subscription createFromParcel(Parcel parcel) {
+            return new Subscription(parcel);
+        }
+
+        @Override
+        public Subscription[] newArray(int i) {
+            return new Subscription[i];
+        }
+    };
+
     private String mTitle;
     private String mUrl;
+
+    public Subscription() {}
+
+    public Subscription(Parcel in) {
+        mTitle = in.readString();
+        mUrl = in.readString();
+    }
 
     public String getmTitle() {
         return mTitle;
@@ -18,5 +41,16 @@ public class Subscription {
 
     public void setmUrl(String mUrl) {
         this.mUrl = mUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int i) {
+        out.writeString(mTitle);
+        out.writeString(mUrl);
     }
 }
