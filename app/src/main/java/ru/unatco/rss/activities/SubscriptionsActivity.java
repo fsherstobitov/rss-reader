e67bff3d55751754dea23dc1e9f5b687e873d46e
frodo <frodo@z-inc.net>
2015-06-30 14:25:52 +0500

    moved SubscriptionsActivity to activities package

--- app/src/main/java/ru/unatco/rss/activities/SubscriptionsActivity.java
+++ app/src/main/java/ru/unatco/rss/activities/SubscriptionsActivity.java
@@ -1,9 +1,11 @@
-package ru.unatco.rss;
+package ru.unatco.rss.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import ru.unatco.rss.R;
 
 
 public class SubscriptionsActivity extends AppCompatActivity {
