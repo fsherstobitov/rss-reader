53ad483594f6a2a3f86cc6107d19a41e081a9697
frodo <frodo@z-inc.net>
2015-06-30 15:10:08 +0500

    added getters and setters for Subscription model

--- app/src/main/java/ru/unatco/rss/model/Subscription.java
+++ app/src/main/java/ru/unatco/rss/model/Subscription.java
@@ -3,4 +3,20 @@ package ru.unatco.rss.model;
 public class Subscription {
     private String mTitle;
     private String mUrl;
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
 }
