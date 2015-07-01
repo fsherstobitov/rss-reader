d4a7a4bf1357dc38c9466e2f33bfaf605840cc0b
Fedor Sherstobitov <fsherstobitov@gmail.com>
2015-07-01 13:42:14 +0500

    added ItemActivity with layout

---
+++ app/src/main/java/ru/unatco/rss/activities/ItemActivity.java
@@ -0,0 +1,66 @@
+package ru.unatco.rss.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ru.unatco.rss.R;
+import ru.unatco.rss.model.Item;
+
+public class ItemActivity extends AppCompatActivity {
+
+    public static final String ARG_ITEM = "ru.unatco.rss.Item";
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.web_view)
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Item item = getIntent().getParcelableExtra(ARG_ITEM);
+        getSupportActionBar().setTitle(item.getmTitle());
+        mWebView.loadData(item.getmDescription(), null, null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
