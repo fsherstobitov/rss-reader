648efa453b3828768a244b975092cc46cbd993fe
Fedor Sherstobitov <fsherstobitov@gmail.com>
2015-07-06 16:02:29 +0500

    added RssDatabaseHelper

---
+++ app/src/main/java/ru/unatco/rss/data/local/RssDatabaseHelper.java
@@ -0,0 +1,28 @@
+package ru.unatco.rss.data.local;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class RssDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "rss.sqlite";
+    private static final int VERSION = 1;
+
+    public RssDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table subscriptions (_id integer primary key autoincrement, title text, url text)");
+        sqLiteDatabase.execSQL("create table items (_id integer primary key autoincrement, title text, description text, sub_id integer references subscriptions(_id))");
+
+        sqLiteDatabase.execSQL("insert into subscriptions values ('Радио-Т', 'http://feeds.rucast.net/radio-t')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
